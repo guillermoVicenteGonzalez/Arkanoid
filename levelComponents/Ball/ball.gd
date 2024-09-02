e950448b34f5cd164b78f6fd44b6777ca014ee9c
guillermoVicenteGonzalez <guillermovg@usal.es>
2024-09-02 17:14:30 +0200

    ball tool

--- levelComponents/Ball/ball.gd
+++ levelComponents/Ball/ball.gd
@@ -5,15 +5,22 @@ class_name Ball extends RigidBody2D
 @onready var ball_collision: CollisionShape2D = %ballCollision
 @onready var area_shape: CollisionShape2D = %areaShape
 
+@export var sphereColor:Color = Color.WHITE : set = setColor
 @export var speed:float = 350 : set = setSpeed
-@export var shape:Shape2D : set = setShape
+#@export var shape:CircleShape2D: set = setShape
 
+var _shape := CircleShape2D.new() : set = _setShape
+@export var radius:float = 0 : set = setRadius
 var direction:Vector2 = Vector2(0,1)
 
 
 func _physics_process(delta: float) -> void:
 	if not Engine.is_editor_hint():
 		linear_velocity = handleMovement(speed, direction, delta)
+
+
+func _draw() -> void:
+	draw_circle(Vector2(0,0),radius,sphereColor, true)
 
 	
 func handleMovement(sp:float, dir:Vector2, _delta:float)->Vector2:
@@ -25,8 +32,11 @@ func setSpeed(s:float):
 	speed = s
 	
 
-func setShape(nShape:Shape2D):
-	shape = nShape
+func _setShape(nShape:CircleShape2D):
+	_shape = nShape
+	radius = _shape.radius
+	queue_redraw()
+	
 	# include drawn shape code here
 	if ball_collision != null:
 		ball_collision.shape = nShape
@@ -35,9 +45,22 @@ func setShape(nShape:Shape2D):
 		area_shape.shape = nShape
 	
 
+func setColor(nColor:Color):
+	sphereColor = nColor
+	queue_redraw()
+	print_debug("queue redraw ?")
+
+
+func setRadius(nRadius:float):
+	radius = nRadius
+	_shape.radius = radius
+	_shape = _shape
+	queue_redraw()
+
+	
+
 
 func _on_ball_area_body_entered(body: Node2D) -> void:
 	print_debug(body)
 	if body is Player:
 		direction = Vector2(0,-1)
-	pass # Replace with function body.
