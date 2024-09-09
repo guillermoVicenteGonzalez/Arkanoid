48a56d7ffdc248d9c7738ba45a98ba2ef5aaaf7c
guillermoVicenteGonzalez <guillermovg@usal.es>
2024-09-09 13:33:01 +0200

    walls creation

--- levels/template/level.gd
+++ levels/template/level.gd
@@ -1,9 +1,13 @@
 class_name Level extends Node2D
 
+
 @export var size := Vector2(1280, 800)
 
 var speed:float = 1
 var score:int = 0
+
+func _ready() -> void:
+	initializeLevel()
 
 static func instantiateLevel(levelSize:Vector2)-> Level:
 	var l := Level.new()
@@ -12,9 +16,10 @@ static func instantiateLevel(levelSize:Vector2)-> Level:
 
 
 func initializeLevel():
-	# Set dimensions
+	# Set dimensions (camera)
 	# Set player in the middle
 	# Set walls
+	createWalls(size)
 	# Set "goal"
 	# Set blocks
 	pass
@@ -34,3 +39,14 @@ func _checkLevelFinished():
 	# we get elements in group blocks
 	# if it is 0 => _levelFinished()
 	pass
+
+func createWalls(levelSize:Vector2):
+	print_debug("creating walls")
+	var wallSize = Vector2(1,levelSize.y)
+	var leftWall := Wall.instantiateWall(Vector2(0,0))
+	var rightWall := Wall.instantiateWall(Vector2(levelSize.x, 0))
+	add_child(leftWall)
+	add_child(rightWall)
+	leftWall.size = wallSize
+	rightWall.size = wallSize
+	
